da02ead2a9de8a8c26b12b80b0bbd75fc9b78f9e
Erick Ballas <erick.ballas@epn.edu.ec>
2025-05-16 15:30:35 -0500

    FIRST WORK

--- app/src/main/java/com/erick/ballas/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/erick/ballas/cazarpatos/LoginActivity.kt
@@ -1,20 +1,30 @@
 package com.erick.ballas.cazarpatos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private lateinit var btnLogin:Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        btnLogin = findViewById(R.id.btnLogin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Pantalla2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+            }
+            btnLogin.setOnClickListener{
+            startActivity(Intent(this,Pantalla2::class.java))
         }
+
     }
+
 }
