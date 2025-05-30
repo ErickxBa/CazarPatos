7b2c46648aab9e315de462aad072b5028e77aecf
P3E004-K <luis.rocha@epn.edu.ec>
2025-05-30 15:25:21 -0500

    Revert "LOGIN AND MAIN_ACTIVITY UPDATE"
    This reverts commit 752dbd379832cfedb623d93d5f57be1115d98318.

--- app/src/main/java/com/erick/ballas/cazarpatos/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.erick.ballas.cazarpatos
-
-const val EXTRA_LOGIN = "EXTRA_LOGIN"
--- app/src/main/java/com/erick/ballas/cazarpatos/Pantalla2.kt
+++ app/src/main/java/com/erick/ballas/cazarpatos/Pantalla2.kt
@@ -1,30 +1,20 @@
 package com.erick.ballas.cazarpatos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var btnLogin:Button;
+class Pantalla2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        btnLogin = findViewById(R.id.btnLogin)
+        setContentView(R.layout.activity_pantalla2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Pantalla2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            }
-            btnLogin.setOnClickListener{
-            startActivity(Intent(this,Pantalla2::class.java))
         }
-
     }
-
 }
