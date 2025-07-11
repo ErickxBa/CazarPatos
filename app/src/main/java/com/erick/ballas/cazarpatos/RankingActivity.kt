13f5b5f78c860f6b56946e013b73c0d5ae480602
ErickxBa <ballassebastian@gmail.com>
2025-07-11 15:41:05 -0500

    Implement SQLite database for player ranking

--- app/src/main/java/com/erick/ballas/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/erick/ballas/cazarpatos/RankingActivity.kt
@@ -16,6 +16,12 @@ class RankingActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+
+        OperacionesSqLite()
+        GrabarRankingSQLite()
+        LeerRankingsSQLite()
+
+
         var jugadores = arrayListOf<Player>()
         jugadores.add(Player("Erick.Ballas",10))
         jugadores.add(Player("Jugador2",6))
@@ -29,4 +35,34 @@ class RankingActivity : AppCompatActivity() {
         recyclerViewRanking.setHasFixedSize(true);
 
     }
+
+    fun OperacionesSqLite(){
+        RankingPlayerDBHelper(this).deleteAllRanking()
+        RankingPlayerDBHelper(this).insertRankingByQuery(Player("Jugador9",10))
+        val patosCazados = RankingPlayerDBHelper(this).readDucksHuntedByPlayer("Jugador9")
+        RankingPlayerDBHelper(this).updateRanking(Player("Jugador9",5))
+        RankingPlayerDBHelper(this).deleteRanking("Jugador9")
+        RankingPlayerDBHelper(this).insertRanking(Player("Jugador9",7))
+        val players = RankingPlayerDBHelper(this).readAllRankingByQuery()
+    }
+    fun GrabarRankingSQLite(){
+        val jugadores = arrayListOf(
+            Player("Erick.Ballas", 21),
+            Player("Jugador2", 6),
+            Player("Jugador3", 3),
+            Player("Jugador4", 9)
+        )
+        jugadores.sortByDescending { it.huntedDucks }
+        for(jugador in jugadores){
+            RankingPlayerDBHelper(this).insertRanking(jugador)
+        }
+    }
+    fun LeerRankingsSQLite(){
+        val jugadoresSQLite = RankingPlayerDBHelper(this).readAllRanking()
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking)
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
+        recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
+        recyclerViewRanking.setHasFixedSize(true)
+    }
+
 }
